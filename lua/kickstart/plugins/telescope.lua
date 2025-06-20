c83569ac1d3b771bf4374742ef1c5b5946116f73
yuzunosk55 <yuzuha3ds@gmail.com>
2025-06-20 13:20:21 +0900

    keymapの説明をを日本語化

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -67,16 +67,16 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S][H]ヘルプ検索' })
+    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S][K]キーマップ検索' })
+    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S][F]ファイル検索' })
+    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]Telescope[S]選択' })
+    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]現在の[W]単語を検索' })
+    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S][G]rep検索' })
+    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S][D]診断検索' })
+    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S][R]検索再開' })
+    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]最近のファイル検索' })
+    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] 既存バッファ検索' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
@@ -85,7 +85,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
         winblend = 10,
         previewer = false,
       })
-    end, { desc = '[/] Fuzzily search in current buffer' })
+    end, { desc = '[/] 現在のバッファでファジー検索' })
 
     -- It's also possible to pass additional configuration options.
     --  See `:help telescope.builtin.live_grep()` for information about particular keys
@@ -94,12 +94,12 @@ return { -- Fuzzy Finder (files, lsp, etc)
         grep_open_files = true,
         prompt_title = 'Live Grep in Open Files',
       }
-    end, { desc = '[S]earch [/] in Open Files' })
+    end, { desc = '[S]開いているファイルで[/]検索' })
 
     -- Shortcut for searching your Neovim configuration files
     vim.keymap.set('n', '<leader>sn', function()
       builtin.find_files { cwd = vim.fn.stdpath 'config' }
-    end, { desc = '[S]earch [N]eovim files' })
+    end, { desc = '[S][N]eovimファイル検索' })
   end,
 }
 
